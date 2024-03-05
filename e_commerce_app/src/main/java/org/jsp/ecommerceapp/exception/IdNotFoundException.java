49bffdc2938ae880295556584860c6bae834d6bd
ramu1105 <ramuramum773@gmail.com>
2024-03-05 16:29:08 +0530

    added e_commerce_app

---
+++ e_commerce_app/src/main/java/org/jsp/ecommerceapp/exception/IdNotFoundException.java
@@ -0,0 +1,11 @@
+package org.jsp.ecommerceapp.exception;
+
+public class IdNotFoundException extends RuntimeException{
+
+	@Override
+	public String getMessage() {
+		return "Invalid Id";
+	}
+	
+
+}
